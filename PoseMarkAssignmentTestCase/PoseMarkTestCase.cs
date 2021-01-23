478d2d3861d4059a1b7fb1b99313fcf35b6c4b67
Dilli Babu <payamthd@ES.AD.ADP.COM>
2021-01-23 23:48:16 +0530

    Adding Unit testing Project

---
+++ PoseMarkAssignmentTestCase/PoseMarkTestCase.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+using PoshMarkAssignment;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PoseMarkAssignmentTestCase
+{
+    public class PoseMarkTestCase
+    {
+        [Fact]
+        public void Test1()
+        {
+            string output = "[{\"Region\":\"us-east\",\"Price\":5.64,\"Server\":[{\"Item1\":\"x10large\",\"Item2\":1}]},{\"Region\":\"us-west\",\"Price\":5.94,\"Server\":[{\"Item1\":\"x10large\",\"Item2\":1}]},{\"Region\":\"asia\",\"Price\":4.72,\"Server\":[{\"Item1\":\"x8large\",\"Item2\":2}]}]";
+            List<ResponseObject> actual = JsonConvert.DeserializeObject<List<ResponseObject>>(output);
+            List<ResponseObject> expected = Program.Get_cost(2, 32, 8);
+            Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+    }
+}
